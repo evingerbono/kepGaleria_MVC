1cf2a705b5d5e4cb6785b877e98146634935f1c9
evingerbono <bono0823@gmail.com>
2023-09-26 11:40:32 +0200

    Kép_léptetése

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -4,6 +4,15 @@ class Controller{
     constructor(){
         const MODEL = new Model();
         const NAGYK = new NagyKep($(".nagyk"),MODEL.getAktKep());
+
+        $(window).on("jobb", () => {
+            MODEL.jobb();
+            NAGYK.nagyKepCsere(MODEL.getAktKep());
+        })
+        $(window).on("bal", () => {
+            MODEL.bal();
+            NAGYK.nagyKepCsere(MODEL.getAktKep());
+        })
     }
 }
 export default Controller;
--- View/NagyKepView.js
+++ View/NagyKepView.js
@@ -5,16 +5,40 @@ class NagyKepView {
         this.#kep = kep;
         this.szuloElem = szuloElem;
         this.#htmlOsszerak();
+        this.balGomb = $(".bal");
+        this.balGomb.on("click", () => {
+            this.#esemenyTrigger("bal");
+            console.log("Bal");
+        });
+        this.jobbGomb = $(".jobb");
+        this.jobbGomb.on("click", () => {
+            this.#esemenyTrigger("jobb");
+            console.log("Jobb");
+        });
+        this.nagyKepHely=$("#nagyKepHely");
     }
 
     #htmlOsszerak() {
         let txt = "";
         txt += `<div class="card">
-        <div class="card-header">Főkép</div>
-        <div class="card-body"><img src="${this.#kep}" class="img-thumbnail" alt="Virag"></div>
-        <div class="card-footer">Leírás</div>
-      </div>`;
-      this.szuloElem.html(txt);
+        <div class="card-header text-center">Főkép</div>
+        <div class="card-body text-center">
+        <button type="button" class="btn btn-danger bal">Bal</button>
+        <img id="nagyKepHely" src="${this.#kep}" class="img-thumbnail" alt="Virag">
+        <button type="button" class="btn btn-danger jobb">Jobb</button>
+        </div>
+        <div class="card-footer text-center">Leírás</div>
+        </div>`;
+        this.szuloElem.html(txt);
+    }
+
+    nagyKepCsere(kep){
+        this.nagyKepHely.attr("src",kep);
+    }
+
+    #esemenyTrigger(esemenyNev) {
+        const esemeny = new CustomEvent(esemenyNev);
+        window.dispatchEvent(esemeny);
     }
 }
 export default NagyKepView;
